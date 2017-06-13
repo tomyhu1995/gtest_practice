2dd042f7abec412ef1819afe3c61e3d4ca881040
Tommy Hu <tomyhu1995@gmail.com>
2017-06-14 01:39:18 +0800

    First upload

---
+++ gtest_practice.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <iostream>
+  
+int MyAdd(int a, int b) {
+    return a + b;
+}
+  
+TEST(AddTest, PositiveValue) {
+    EXPECT_EQ(3, MyAdd(1, 2));
+}
+  
+TEST(AddTest, NegativeValue) {
+    EXPECT_EQ(-5, MyAdd(-2, -3));
+}
+  
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
